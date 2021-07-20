533557ae4a496aae1f62db2b97601ef720f8d942
nashv <nashvegasnate@gmail.com>
2021-07-20 18:18:39 -0500

    Add project files.

---
+++ PetStore/Pets/Dog.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetStore.Pets
+{
+    class Dog //implicitly internal
+    {
+        //Access Modifiers:
+        //private: only things in this class can use it-DEFAULT IN A CLASS
+        //public: everything can access it
+        //internal: anything in this project can access it
+        //protected: anything that inherits from me can access it
+
+        //Properties
+        //1)access modifier 2)type 3)name {}
+        public string Size { get; set; } //auto-property
+        public short Weight { get; private set; }
+        public string Name { get; set; }
+
+        //constructor or constructor method
+        public Dog(string name, short weight, string size)
+        {
+            Name = name;
+            Weight = weight;
+            Size = size;
+        }
+        //Method Signature: 1)access modifier, 2)return type, 3)name, 4)parameters
+        //void means not returning anything
+        public void Bark()
+        {
+            Console.WriteLine($"{Name} barks noisily.");
+        }
+
+        //parameters are camelCase
+        public void Eat(string typeOfFood)
+        {
+            //if (string.Equals(typeOfFood, "healthy food", StringComparison.OrdinalIgnoreCase)) OR
+            if (typeOfFood.ToLower() == "healthy food")
+            {
+                Weight -= 1;
+            }
+
+            Console.WriteLine($"{Name} just ate {typeOfFood}. {Name} weighs {Weight}");
+        }
+    }
+}
---
+++ PetStore/Program.cs
@@ -0,0 +1,37 @@
+﻿using PetStore.Pets;
+using System;
+
+namespace PetStore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var melba = new Dog("Melba", 60, "Medium");
+            //melba.Name = "Melba";
+           // melba.Size = "Medium";
+           // melba.Weight = 60;
+            melba.Bark();
+
+            //property is privately settable only
+            //melba.Weight -= 15;
+
+            Console.WriteLine($"What kind of food should {melba.Name} eat");
+            var typeOfFoodForMelba = Console.ReadLine();
+
+            melba.Eat("Healthy food");
+
+            //this won't work- can't dynamically add properties to a class in c#
+            //melba.CoatType = "Short";
+
+            var barley = new Dog("Barley", 50, "Medium");
+            //barley.Name = "Barley";
+            //barley.Size = "Medium";
+            //barley.Weight = 50;
+            barley.Bark();
+            barley.Eat("Table scraps");
+
+     
+        }
+    }
+}
